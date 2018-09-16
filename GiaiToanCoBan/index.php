90e272bf2a2ab2eec17e091a98338f83c4d7b255
phuctrandai <trandaiphuc.1501@gmail.com>
2018-09-16 10:14:44 +0700

    Tạo giao diện các cho các tr

---
+++ GiaiToanCoBan/index.php
@@ -0,0 +1,23 @@
+
+    <div class="bg-dark" style="width:100%; height: 50px">
+        <p style="color:#fff; font-weight: bold; padding: 15px 15px;">Toán học cơ bản</p>
+    </div>
+    <nav class="navbar bg-dark" id="menu">
+        <ul class="navbar-nav">
+            <li class="nav-item <?php if($_SESSION["page"] == 1){ echo "activeLink";} else { echo "";}?>">
+                <a class="nav-link" href="soNguyenTo.php">Số nguyên tố</a>
+            </li>
+            <li class="nav-item <?php if($_SESSION["page"] == 2){ echo "activeLink";} else { echo "";}?>">
+                <a class="nav-link" href="phuongTrinhBacNhat.php">Phương trình bậc nhất</a>
+            </li>
+            <li class="nav-item <?php if($_SESSION["page"] == 3){ echo "activeLink";} else { echo "";}?>">
+                <a class="nav-link" href="phuongTrinhBacHai.php">Phương trình bậc hai</a>
+            </li>
+            <li class="nav-item <?php if($_SESSION["page"] == 4){ echo "activeLink";} else { echo "";}?>">
+                <a class="nav-link" href="chuViVaDienTichTamGiac.php">Chu vi và diện tích tam giác</a>
+            </li>
+            <li class="nav-item <?php if($_SESSION["page"] == 5){ echo "activeLink";} else { echo "";}?>">
+                <a class="nav-link" href="giaiThua.php">Giai thừa</a>
+            </li>
+        </ul>
+    </nav>
---
+++ GiaiToanCoBan/phuongTrinhBacNhat.php
@@ -0,0 +1,21 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Toán học cơ bản</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        
+        <link rel="stylesheet" href="./CSS/style.css">
+    </head>
+    <body>
+        <?php
+            $_SESSION['page'] = 2;
+            include_once './index.php';
+        ?>
+    </body>
+</html>
